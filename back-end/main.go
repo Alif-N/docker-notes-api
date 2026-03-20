f8495c9851a2360d2a4bbaa3d3c73c8feee04a18
Alif-N <alifnasrullah05@gmail.com>
2026-03-20 22:31:12 +0700

    initial project notes api with docker service

---
+++ back-end/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"notes-api/db"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// 🔥 connect DB
+	db.Connect()
+
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "API is running 🚀",
+		})
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Println("Server running on port", port)
+	r.Run(":" + port)
+}
